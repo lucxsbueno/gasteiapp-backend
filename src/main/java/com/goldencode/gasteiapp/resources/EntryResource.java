4de2e95a274478f125e28ffd92ab48b351593455
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2021-02-03 12:24:37 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/goldencode/gasteiapp/resources/EntryResource.java
+++ src/main/java/com/goldencode/gasteiapp/resources/EntryResource.java
@@ -1,16 +1,29 @@
 package com.goldencode.gasteiapp.resources;
 
+import com.goldencode.gasteiapp.domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping(value="/entries")
+@RequestMapping(value="/categories")
 public class EntryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String welcome() {
-        return "Welcome to my Api REST.";
+    public List<Category> welcome() {
+
+        Category  cat1 = new Category(1, "Merch");
+        Category  cat2 = new Category(2, "Shopping");
+
+        List<Category> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+        
+        return list;
     }
 
 }

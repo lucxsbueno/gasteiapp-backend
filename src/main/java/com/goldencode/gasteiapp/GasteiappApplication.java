9a81ea1873f78ed6702135873e6a478fb68eb093
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2021-02-03 11:34:01 -0300

    Projeto criado

---
+++ src/main/java/com/goldencode/gasteiapp/GasteiappApplication.java
@@ -0,0 +1,13 @@
+package com.goldencode.gasteiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasteiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasteiappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldencode/gasteiapp/GasteiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldencode.gasteiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasteiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

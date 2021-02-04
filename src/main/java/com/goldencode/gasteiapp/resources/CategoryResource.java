d9831c7226b3dda938ee8208390cc2e2f84784bf
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2021-02-04 17:36:01 -0300

    Atualizando uma categoria e buscando todas as categorias

--- src/main/java/com/goldencode/gasteiapp/resources/CategoryResource.java
+++ src/main/java/com/goldencode/gasteiapp/resources/CategoryResource.java
@@ -4,10 +4,11 @@ import com.goldencode.gasteiapp.domain.Category;
 import com.goldencode.gasteiapp.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categories")
@@ -18,11 +19,28 @@ public class CategoryResource {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id) {
-
         Category obj = service.find(id);
-
         return ResponseEntity.ok().body(obj);
+    }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody Category obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> findAll() {
+        List<Category> obj = service.findAll();
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody Category obj, @PathVariable Integer id) {
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/com/goldencode/gasteiapp/services/CategoryService.java
+++ src/main/java/com/goldencode/gasteiapp/services/CategoryService.java
@@ -6,6 +6,7 @@ import com.goldencode.gasteiapp.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,21 @@ public class CategoryService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado: "+id+
                 ", Tipo: "+Category.class.getName()));
     }
+
+    public Category insert(Category obj) {
+        obj.setId(null);
+        return repo.save(obj);
+    }
+
+    public List<Category> findAll(){
+        return repo.findAll();
+    }
+
+    public Category update(Category obj) {
+        find(obj.getId());
+        return repo.save(obj);
+    }
+
+
 
 }
